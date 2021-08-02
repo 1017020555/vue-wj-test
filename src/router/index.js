376c085f8cf15da696e1f2dbb9e7a4fd04195933
“lichao” <1017020555@qq.com>
2021-08-02 22:40:28 +0800

    导航栏 + NavMenu

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Login from '@/components/Login'
 import AppIndex from '@/components/home/AppIndex'
+import Home from "../components/Home";
 
 Vue.use(Router)
 
@@ -20,12 +21,22 @@ export default new Router({
       component: Login
     },
     {
-      path: '/index',
-      name: 'AppIndex',
-      component: AppIndex,
-      meta: {
-        requireAuth: true
-      }
+      path: '/home',
+      name: 'Home',
+      component: Home,
+      // home页面并不需要被访问
+      redirect:'/index',
+      children: [
+        {
+          path: '/index',
+          name: 'AppIndex',
+          component: AppIndex,
+          meta: {
+            requireAuth: true
+          }
+        }
+      ]
+
     }
 
   ]
