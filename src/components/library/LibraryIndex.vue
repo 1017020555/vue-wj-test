88c5a59caf1a56e003aba009251389aa04a0ecb5
“lichao” <1017020555@qq.com>
2021-08-04 22:24:32 +0800

    LibraryIndex.vue+SideMenu.vue

---
+++ src/components/library/LibraryIndex.vue
@@ -0,0 +1,26 @@
+<template>
+  <el-container>
+
+    <el-aside >
+      <switch></switch>
+      <side-menu></side-menu>
+    </el-aside>
+
+    <el-main >
+      Main
+    </el-main>
+
+  </el-container>
+</template>
+
+<script>
+    import SideMenu from "./SideMenu";
+    export default {
+      name: "LibraryIndex",
+      components: {SideMenu}
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import HelloWorld from '@/components/HelloWorld'
 import Login from '@/components/Login'
 import AppIndex from '@/components/home/AppIndex'
 import Home from "../components/Home";
+import LibraryIndex from "../components/library/LibraryIndex";
 
 Vue.use(Router)
 
@@ -31,6 +32,14 @@ export default new Router({
           path: '/index',
           name: 'AppIndex',
           component: AppIndex,
+          meta: {
+            requireAuth: true
+          }
+        },
+        {
+          path: '/library',
+          name: 'LibraryIndex',
+          component: LibraryIndex,
           meta: {
             requireAuth: true
           }
