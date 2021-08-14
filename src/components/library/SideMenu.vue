7cfe370de03a658a39a4753c28bcbe947a733552
“lichao” <1017020555@qq.com>
2021-08-14 21:57:13 +0800

    前端增删改查

--- src/components/library/SideMenu.vue
+++ src/components/library/SideMenu.vue
@@ -36,17 +36,29 @@
 </template>
 
 <script>
-    export default {
-        name: "SideMenu",
-        methods: {
-          handleSelect(key, keyPath) {
-            // console.log(key, keyPath);
-            // console.log($route.path)
-          }
-        }
+  export default {
+    name: 'SideMenu',
+    data () {
+      return {
+        cid: ''
+      }
+    },
+    methods: {
+      handleSelect (key, keyPath) {
+        this.cid = key
+        this.$emit('indexSelect')
+      }
     }
+  }
 </script>
 
 <style scoped>
-
+  .categories {
+    position: fixed;
+    margin-left: 50%;
+    left: -600px;
+    top: 100px;
+    width: 150px;
+  }
 </style>
+
